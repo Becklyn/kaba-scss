3823e0a8626f2786a26eb59c9b763e6e289c7487
Jannik Zschiesche <jz@becklyn.com>
2020-01-07 21:25:44 +0100

    Fix invalid return value check + improve output of test-helper

--- tests/helpers/test-helper.js
+++ tests/helpers/test-helper.js
@@ -1,6 +1,7 @@
 import test from "ava";
 import {KabaScss} from "../../src/index";
 import {MemoryLogger} from "../../src/Logger/MemoryLogger";
+import {gray} from "kleur";
 const path = require("path");
 const fs = require("fs-extra");
 
@@ -12,9 +13,11 @@ const fs = require("fs-extra");
 export function testScssCompilation (scssFilePath, options = {})
 {
     let relativeFilePath = path.relative(process.cwd(), scssFilePath);
+    options.lint = true;
 
     test(
         `Compile SCSS file '${relativeFilePath}' with options: ${JSON.stringify(options)}`,
+        /** @var {ava.Assertions} t */
         async t =>
         {
             const outputDir = path.join(__dirname, ".dist");
@@ -25,10 +28,22 @@ export function testScssCompilation (scssFilePath, options = {})
 
             compiler.addEntry(scssFilePath, outputDir);
             let result = await compiler.run();
+            let output = logger.getOutput();
 
-            t.is(result, false);
-            t.regex(logger.getOutput(), /Build started/g);
-            t.regex(logger.getOutput(), /Build finished/g);
+            if (true === result && /Build started/.test(output) && /Build finished/.test(output))
+            {
+                t.pass("Result ok, and build seems to have finished successfully");
+            }
+            else
+            {
+                t.fail(
+                    "Test failed with output:\n" +
+                    output.split("\n").map(l => `${gray("|")}    ${l}`).join("\n") +
+                    "\n"
+                );
+            }
+
+
 
             // clean
             fs.removeSync(outputDir);
